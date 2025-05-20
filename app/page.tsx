a4b7e8264a85304f0843093a55a87357645492e8
vladlen25 <vladlenkuznetsov203@gmail.com>
2025-05-20 19:35:34 +0200

    added BarChart to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
+import AppBarChart from "@/components/charts/AppBarChart";
 
 const Homepage = () => {
   return (
       <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
         <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
-          text
+          <AppBarChart/>
         </div>
         <div className="bg-primary-foreground p-4 rounded-lg">
           text
